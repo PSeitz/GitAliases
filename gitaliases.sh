4ca7b136b805ff361fa6467e178e06246b71c86b
PSeitz <pascal.seitz@gmail.com>
2014-08-03 11:36:06 +0200

    Update gitaliases.sh

--- gitaliases.sh
+++ gitaliases.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git config --global alias.s status
 git config --global alias.f fetch
