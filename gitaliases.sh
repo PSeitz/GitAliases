027ec989566b837360fe035bc6b2957c84e19ffe
PSeitz <pascal.seitz@gmail.com>
2014-08-03 11:44:02 +0200

    Update gitaliases.sh

--- gitaliases.sh
+++ gitaliases.sh
@@ -1,10 +1,1 @@
-set -e
-
 echo "WHOA"
-
-git config --global alias.s status
-git config --global alias.f fetch
-git config --global alias.bam push
-git config --global alias.r rebase
-git config --global alias.m mergetool
-git config --global alias.c commit
